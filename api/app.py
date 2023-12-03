15459fb67b24c38c66028b0ad483991954ac3500
sf23 <sf23@imperial.ac.uk>
2023-12-03 10:40:39 +0000

    week 1 version 1

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 import math
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 from lxml import html
 import requests
-from flask import Flask, render_template, request
-import psycopg2 as db
+import psycopg as db
 import uuid
+# import psycopg2 as db
+import hashlib
+from openpyxl import Workbook
 
 # from flask_mail import Mail, Message
 # from itsdangerous import URLSafeTimedSerializer
@@ -18,24 +20,75 @@ import uuid
 # mail = Mail(app)
 
 app = Flask(__name__)
+app.secret_key = 'your_unique_and_secret_key'
 
 
+# 连接 MySQL 数据库
+def get_db_connection():
+    # server_params = {
+    #     'dbname': 'nl1023',
+    #     'host': 'db.doc.ic.ac.uk',
+    #     'port': '5432',
+    #     'user': 'nl1023',
+    #     'password': 'aFZK-3CzFH*j3y',
+    #     'client_encoding': 'utf-8'
+    # }
+    server_params = {'dbname': 'sf23',
+                 'host': 'db.doc.ic.ac.uk',
+                 'port': '5432',
+                 'user': 'sf23',
+                 'password': '3048=N35q4nEsm',
+                 'client_encoding':'utf-8'}
+    return db.connect(**server_params)
+
+# homepage route for the app
 @app.route("/")
 def hello_world():
-    return render_template("compare.html")
-    # return "Hello , my new app!"
+    return render_template("index.html")
 
 @app.route("/keywordsubmit", methods=['POST'])
 def keywordsubmit():
     keyword = request.form.get("keyword")
-    
-    # return keyword
+    products_list = send_request(keyword)
+    # if(products_list):
 
+    result2=vipapi()
+    # print(products_list[0])
+    return render_template("compare.html", result1 = products_list[0], result2=result2)
+    return products_list[0]
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
+# 处理登录请求
+@app.route("/login", methods=["POST"])
+def submit():
+    data = request.json  # 获取 JSON 数据
 
-    products_list = send_resuest(keyword)
-    return products_list
+    # 获取用户名和密码
+    username = data.get("username")
+    password = data.get("password")
+
+    # 连接数据库
+    conn = get_db_connection()
+
+    # 创建游标对象
+    cursor = conn.cursor()
+
+    # 查询数据库中是否存在该用户
+    query = "SELECT * FROM my_user WHERE name = %s"
+    cursor.execute(query, (username,))
+    user = cursor.fetchone()
+    print(password,user[6])
+    conn.close()
+    # if user and hashlib.md5(password.encode()).hexdigest()==user[6]:  
+    if user and password==user[6]:
+        session['user'] = username
+        return jsonify({'message': 'Login successful'})
+        # return render_template("compare.html", result1={})
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,13 +110,19 @@ def register():
 
         values = (user_id, full_name, gender, email, dob, postcode, password)
 
-        server_params = {
-            'dbname': 'kac23',
-            'host': 'db.doc.ic.ac.uk',
-            'port': '5432',
-            'user': 'kac23',
-            'password': '3E13Nt3,SX'
-        }
+        # server_params = {
+        #     'dbname': 'kac23',
+        #     'host': 'db.doc.ic.ac.uk',
+        #     'port': '5432',
+        #     'user': 'kac23',
+        #     'password': '3E13Nt3,SX'
+        # }
+        server_params = {'dbname': 'sf23',
+                 'host': 'db.doc.ic.ac.uk',
+                 'port': '5432',
+                 'user': 'sf23',
+                 'password': '3048=N35q4nEsm',
+                 'client_encoding':'utf-8'}
 
         try:
             conn = db.connect(**server_params)
@@ -94,6 +153,59 @@ def register():
     else:
         return render_template("register.html")
 
+# Profile route
+@app.route('/profile')
+def profile():
+    server_params = {'dbname': 'sf23',
+                 'host': 'db.doc.ic.ac.uk',
+                 'port': '5432',
+                 'user': 'sf23',
+                 'password': '3048=N35q4nEsm',
+                 'client_encoding':'utf-8'}
+    if 'user' in session:
+        username = session['user']
+
+        # Connect to the database
+        try:
+            conn = db.connect(**server_params)
+            curs = conn.cursor()
+            # curs.execute(sqlcommand, values)
+            # conn.commit()  # Commit to save changes
+
+            # config=configparser.ConfigParser()
+            # config.read('dbtool.ini')
+
+            # conn = db.connect(**config['connection'])
+            # curs = conn.cursor()
+
+            # Perform the profile query to fetch user data based on the username
+            curs.execute("SELECT * FROM my_user WHERE name = %s", (username,))
+            user = curs.fetchone()
+
+            # Check if the user is found
+            if user:
+                # The user is a tuple; you may want to convert it to a dictionary for easier handling
+                user_dict = {
+                    'username': user[1],
+                    'email': user[3],
+                    'gender': user[2],
+                    'postcode': user[5],
+                    # Add other properties as needed
+                }
+
+                return render_template('profile.html', user=user_dict)
+            else:
+                return 'User not found.'
+        except Exception as e:
+            print(f"An error occurred: {e}")  # Log the error
+            return 'Error fetching profile data.'
+        finally:
+            if 'curs' in locals():
+                curs.close()
+            if 'conn' in locals():
+                conn.close()
+    else:
+        return 'You are not logged in. Please log in.'
 # @app.route('/confirm_email/<token>')
 # def confirm_email(token):
 #     try:
@@ -134,8 +246,11 @@ def register():
 
 #     return render_template("email_confirmation_result.html", message=message)
 
+@app.route("/compare")
+def compare():
+    return render_template("compare.html", result1={}, result2={})
 
-def send_resuest(keyword):
+def send_request(keyword):
     products_list=[]
     """ 爬取京东的商品数据 """
     # url = 'https://search.jd.com/Search?keyword={0}&enc=utf-8'.format(word)
@@ -181,9 +296,121 @@ def send_resuest(keyword):
                 'price': price[0],
                 'link': 'https:' + link[0],
                 'store': store[0],
-                'referer': '京东'
+                'referer': 'JD'
             })
+    
+
     return products_list
+
+
+def vipapi():
+    headers = {
+    'Referer': 'https://category.vip.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+    }
+
+    url = 'https://mapi.vip.com/vips-mobile/rest/shopping/pc/search/product/rank'
+    data = {
+    'app_name': 'shop_pc',
+    'app_version': '4.0',
+    'warehouse': 'VIP_HZ',
+    'fdc_area_id': '104103101',
+    'client': 'pc',
+    'mobile_platform': '1',
+    'province_id': '104103',
+    'api_key': '70f71280d5d547b2a7bb370a529aeea1',
+    'user_id': '',
+    'mars_cid': '1689245318776_e2b4a7b51f99b3dd6a4e6d356e364148',
+    'wap_consumer': 'a',
+    'standby_id': 'nature',
+    'keyword': '耐克运动鞋',
+    'lv3CatIds': '',
+    'lv2CatIds': '',
+    'lv1CatIds': '',
+    'brandStoreSns': '',
+    'props': '',
+    'priceMin': '',
+    'priceMax': '',
+    'vipService': '',
+    'sort': '0',
+    'pageOffset': '0',
+    'channelId': '1',
+    'gPlatform': 'PC',
+    'batchSize': '120',
+    '_': '1689250387620',
+    }
+
+    response = requests.get(url=url, params=data, headers=headers)
+    products = [i['pid'] for i in response.json()['data']['products']]
+
+    # 打开文件并创建 CSV 写入器
+    # 创建一个工作簿和工作表
+    workbook = Workbook()
+    sheet = workbook.active
+
+    # 设置表头
+    header = [
+        '标题', '品牌', '售价', '图片', '商品信息', '详情页'
+    ]
+    sheet.append(header)
+
+    min_price_row=[]
+    for i in range(0, len(products), 50):
+        product_id = ','.join(products[i:i+50])
+        link = 'https://mapi.vip.com/vips-mobile/rest/shopping/pc/product/module/list/v2'
+        params = {
+            # 'callback': 'getMerchandiseDroplets2',
+            'app_name': 'shop_pc',
+            'app_version': '4.0',
+            'warehouse': 'VIP_HZ',
+            'fdc_area_id': '104103101',
+            'client': 'pc',
+            'mobile_platform': '1',
+            'province_id': '104103',
+            'api_key': '70f71280d5d547b2a7bb370a529aeea1',
+            'user_id': '',
+            'mars_cid': '1689245318776_e2b4a7b51f99b3dd6a4e6d356e364148',
+            'wap_consumer': 'a',
+            'productIds': product_id,
+            'scene': 'search',
+            'standby_id': 'nature',
+            'extParams': '{"stdSizeVids":"","preheatTipsVer":"3","couponVer":"v2","exclusivePrice":"1","iconSpec":"2x","ic2label":1,"superHot":1,"bigBrand":"1"}',
+            'context': '',
+            '_': '1689250387628',
+        }
+        json_data = requests.get(url=link, params=params, headers=headers).json()
+
+        for index in json_data['data']['products']:
+            attr = ','.join([j['value'] for j in index['attrs']])
+            row = [
+                index['title'],
+                index['brandShowName'],
+                index['price']['salePrice'],
+                index['squareImage'],
+                attr,
+                f'https://detail.vip.com/detail-{index["brandId"]}-{index["productId"]}.html',
+            ]
+            if len(min_price_row)==0 or float(row[2])<float(min_price_row[2]):
+                min_price_row=row
+            sheet.append(row)
+            
+    # 保存数据到Excel文件
+    workbook.save('商品.xlsx')
+    print(min_price_row[0])
+
+    '''
+                        <th>title</th>
+                        <th>link</th>
+                        <th>price</th>
+                        <th>manufacturer</th>
+                        <th>other</th>'''
+    '''
+    ['COURT LEGACY 轻便休闲 小白鞋 男子板鞋',
+    'Nike', 
+    '198',
+    'http://h2.appsimg.com/a.appsimg.com/upload/merchandise/pdcvis/611861/2023/0612/178/4b8049eb-ea43-4631-8e1d-e83f44c4b5f1.jpg', 
+    '板鞋/小白鞋,平底,无,适中', 
+    'https://detail.vip.com/detail-1710618487-6920028890971952983.html']'''
 
 
 
