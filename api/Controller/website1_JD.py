9e5d2dfbd3f091bcdd81cb1317a83c7bf5f85007
sf23 <sf23@imperial.ac.uk>
2023-12-08 23:06:16 +0000

    add cookie when send requests

--- api/Controller/website1_JD.py
+++ api/Controller/website1_JD.py
@@ -1,51 +1,27 @@
+'''
+Author: shihan
+Date: 2023-12-08 22:43:02
+version: 1.0
+description: 
+'''
 import requests
 from lxml import html
 
 
-# SERVER_URL = "your_server_url"
-# accessToken = "your_access_token"
-# appKey = "your_app_key"
-# appSecret = "your_app_secret"
 
-# # Construct the URL
-# url = f"{SERVER_URL}/routerjson"
-
-# # Prepare the request parameters
-# params = {
-#     "method": "jingdong.ware.product.search.list.get",
-#     "accessToken": accessToken,
-#     "appKey": appKey,
-#     "appSecret": appSecret,
-#     "isLoadAverageScore": True,
-#     "isLoadPromotion": True,
-#     "sort": 123,
-#     "page": 123,
-#     "pageSize": 123,
-#     "keyword": "jingdong",
-#     "client": "jingdong"
-# }
-
-# # Make the HTTP GET request
-# response = requests.get(url, params=params)
-
-# # Check the response status code
-# if response.status_code == 200:
-#     # The request was successful, you can parse the response content here
-#     response_data = response.json()
-#     print(response_data)
-# else:
-#     # Handle the error
-#     print(f"Error: {response.status_code} - {response.text}")
 
 
 def send_request_JD(keyword):
     # print("sending")
     products_list = []
     """ 爬取京东的商品数据 """
+    # cookies = dict(thor="A7943E8CDECC88D3F6714286FE2134796C45A5955EA9670F972DBCCBA5D7C0B9F604D0E0CD44219B79DFE31968B2A0D39401BB4812F5E98528728F86ECF2909DB71307217CAE6D4BA887B7206D2CC6850736D97252419EE6DF53B7CD3230A2E6529F3A04E30F5A9EBCBF16FEDE1827759DA5D2C3B8FFA0F955EC9F8161A487DFD66370DF24315D2A853FC04CFA2158C398B378AADC1B28AC199AD9EAA65E602F")
 
-    url = "https://search.jd.com/Search?keyword=" + keyword + "&enc=utf-8"
+    cookies = {'thor':'A7943E8CDECC88D3F6714286FE2134796C45A5955EA9670F972DBCCBA5D7C0B9F604D0E0CD44219B79DFE31968B2A0D39401BB4812F5E98528728F86ECF2909DB71307217CAE6D4BA887B7206D2CC6850736D97252419EE6DF53B7CD3230A2E6529F3A04E30F5A9EBCBF16FEDE1827759DA5D2C3B8FFA0F955EC9F8161A487DFD66370DF24315D2A853FC04CFA2158C398B378AADC1B28AC199AD9EAA65E602F'}
+    # url = "https://search.jd.com/Search?keyword=" + keyword + "&enc=utf-8"
+    url = "https://search.jd.com/Search?keyword=" + keyword
 
-    response = requests.get(url)
+    response = requests.get(url, cookies=cookies)
     # Check the status code
     if response.status_code == 200:
         print("Request was successful!")
@@ -56,9 +32,10 @@ def send_request_JD(keyword):
 
     response.encoding = "utf-8"
     html_doc = response.text
-    print(html_doc)
+    # print(html_doc)
     selector = html.fromstring(html_doc)
 
+    # 所有的商品数据都是通过ul标签进行渲染, 每单个数据，则是用li标签渲染
     ul_list = selector.xpath('//div[@id="J_goodsList"]/ul/li')
     if len(ul_list) == 0:
         products_list.append(
@@ -99,5 +76,8 @@ def send_request_JD(keyword):
                 "referer": "JD",
             }
         )
+        # print()
 
     return products_list
+
+print(send_request_JD("bottle"))
