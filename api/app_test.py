6bd52ddb8626be14ba637519dc9c080b5c1b896b
sf23 <sf23@imperial.ac.uk>
2023-12-09 17:34:24 +0000

    skip translation api test on github(ip issue)

--- api/app_test.py
+++ api/app_test.py
@@ -1,9 +1,9 @@
-'''
+"""
 Author: shihan
 Date: 2023-12-07 21:37:25
 version: 1.0
 description: 
-'''
+"""
 import unittest
 from flask import Flask
 import app
@@ -21,10 +21,10 @@ class TestApp(unittest.TestCase):
         self.assertIn(b"Login", response.data)
 
 
-def test_translate_to_english():
-    assert (
-        translate_to_english("接入举例") == "Access examples"
-    )
+# def test_translate_to_english():
+#     assert (  # test locally, cannot test on github because the api needs authentication of ip, but the github test ip changes each time
+#         translate_to_english("接入举例") == "Access examples"
+#     )
 
 # if __name__ == "__main__":
 #     unittest.main()
